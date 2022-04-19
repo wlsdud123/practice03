7650ba796cf451b88525efa1752e6051a062a549
y <y@DESKTOP-9KEO645>
2022-04-19 10:27:59 +0900

    03

---
+++ src/com/javaex/practice/Ex07.java
@@ -0,0 +1,24 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요");
+		int a = sc.nextInt();
+		
+		for(int i=0; i<=7; i++)
+		{
+		System.out.println(i);
+		i++;
+		}
+		
+		
+
+	}
+
+}
